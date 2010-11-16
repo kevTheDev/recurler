0a9bdcdb5b31b2eab81323c9ddac6bd280f27cdf
Kevin Edwards <kev.j.edwards@gmail.com>
2010-11-16 16:39:08 +0000

    adding hardcoded config options

--- lib/recurler.rb
+++ lib/recurler.rb
@@ -5,9 +5,17 @@ require 'active_resource'
 module Recurler
 
 
-  autoload :Account,        'recurler/account'
-  autoload :BillingInfo,    'recurler/billing_info'
-  autoload :Subscription,   'recurler/subscription'
-  autoload :CreditCard,     'recurler/credit_card'
-  autoload :SubscriptionPlan,     'recurler/subscription_plan'
+  autoload :Account,          'recurler/account'
+  autoload :BillingInfo,      'recurler/billing_info'
+  autoload :Subscription,     'recurler/subscription'
+  autoload :CreditCard,       'recurler/credit_card'
+  autoload :SubscriptionPlan, 'recurler/subscription_plan'
+  
+  
+  # TODO - load this from YAML file at config/recurler.yml
+  self.username  = 'api-test@legallifeguard-test.com'
+  self.site      = 'https://legallifeguard.recurly.com'
+  self.password  = 'ba3b2fae09de4044bcb5c6aa969a7969'
+  self.subdomain = 'legallifeguard-test'
+  
 end
--- lib/recurler/account.rb
+++ lib/recurler/account.rb
@@ -2,6 +2,12 @@ module Recurler
   
   class Account < ActiveResource::Base
     
+    # TODO - load this from YAML file at config/recurler.yml
+    self.username  = 'api-test@legallifeguard-test.com'
+    self.site      = 'https://legallifeguard.recurly.com'
+    self.password  = 'ba3b2fae09de4044bcb5c6aa969a7969'
+    self.subdomain = 'legallifeguard-test'
+    
     include ActiveModel::Serialization
     include ActiveModel::Validations
     include ActiveModel::Conversion
@@ -10,6 +16,10 @@ module Recurler
     attr_accessor :account_code, :username, :first_name, :last_name, :email, :company_name
   
     validates_presence_of :account_code
+  
+    self.element_name = "account"
+    self.primary_key = :account_code
+  
   
     def initialize(attributes = {})
       attributes.each do |name, value|
